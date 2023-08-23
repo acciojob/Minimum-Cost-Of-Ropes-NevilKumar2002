40a082e6d2e0988a144df118aa22c2cee669a2b4
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-23 17:28:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function calculateMinCost() {
   //your code here
+	// preventDefault();
+	let ans=0;
+	let arr=document.getElementById("rope-lengths").value.split(",")
+			.map((val)=>parseInt(val));
+	while(arr.length>1)
+		{
+			arr.sort((a,b)=> a-b);
+			arr[1]+=arr[0];
+			ans+=arr[1];
+			arr.shift();
+		}
+	document.getElementById("result").innerText=ans;
+	
   
   
   
